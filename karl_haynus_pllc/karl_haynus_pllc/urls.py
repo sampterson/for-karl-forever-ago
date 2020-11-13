fadf16b5e87dd76031a8741fbc2f40add80f916d
Sam <peterson.sam.j12@gmail.com>
2020-11-12 20:49:01 -0800

    runs locally

--- karl_haynus_pllc/karl_haynus_pllc/urls.py
+++ karl_haynus_pllc/karl_haynus_pllc/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('home/', include('law_info_app.urls')),
     path('admin/', admin.site.urls),
 ]
+
--- karl_haynus_pllc/karl_haynus_pllc/views.py
+++ karl_haynus_pllc/karl_haynus_pllc/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
+# Create your views here.
 def index(request):
     """View function for home page of site."""
 
