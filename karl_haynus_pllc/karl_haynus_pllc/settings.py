72f34b4c606d59e2a0ff15224e12f1a9e41edbbc
Sam <peterson.sam.j12@gmail.com>
2020-11-12 20:55:49 -0800

    more stuff

--- karl_haynus_pllc/karl_haynus_pllc/settings.py
+++ karl_haynus_pllc/karl_haynus_pllc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ld(8&&zg^!gyn3d(+uno$bljsegiy!ri(uj#1)#irpx%2(j9^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['karl-haynus-law.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
